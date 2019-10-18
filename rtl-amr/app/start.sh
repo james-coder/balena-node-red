7a4d99391f4cd0f461fd8f4f8e9acf2dafe14d7b
James Alton <jamesalton.ut@gmail.com>
2019-10-18 17:01:59 -0600

    Rtl-amr

--- rtl-amr/app/start.sh
+++ rtl-amr/app/start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
 echo "TEST!"
-## Start Xvfb
-#Xvfb -ac -screen scrn 1280x2000x24 :9.0 &
-#export DISPLAY=:9.0
-#
-## Make the default flows available in the user library
-#mkdir -p /data/node-red/user/lib/flows || true
-#cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
-#
-## Start app
-#DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket node-red --settings /usr/src/app/settings.js
+sleep 999999999999999
