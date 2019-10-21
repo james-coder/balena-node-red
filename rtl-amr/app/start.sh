019ffc6560f69f2ac132d56bbf3d667da4042930
James Alton <jamesalton.ut@gmail.com>
2019-10-21 00:33:25 -0600

    Change rtl_tcp path

--- rtl-amr/app/start.sh
+++ rtl-amr/app/start.sh
@@ -3,7 +3,7 @@
 export PERL5LIB=/usr/src/app/perl_modules
 
 echo "RTL-AMR Running!"
-/usr/local/bin/rtl_tcp &
+/usr/bin/rtl_tcp &
 sleep 4
 /home/aptly/go/bin/rtlamr -msgtype=scm -format=json -agcmode=true | /usr/src/app/process_power_usage.pl &
 sleep infinity
